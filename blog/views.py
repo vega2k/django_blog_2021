c44987294a45ed9ec9303749869fd03a9b9aba19
vega2k <vega2k@naver.com>
2021-01-13 16:08:58 +0900

    template extends

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
+
+# Post 상세정보
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# Post 목록
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_list_response(request):
     name = 'Django'
@@ -12,8 +22,3 @@ def post_list_response(request):
     response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
     #return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
     return response
-
-# Post 목록
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
