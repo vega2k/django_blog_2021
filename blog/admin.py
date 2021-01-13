0e2f9ee18faabcc16f238bba0d5c01bcfe7565b8
vega2k <vega2k@naver.com>
2021-01-13 13:59:13 +0900

    template add

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'count_text']
     list_display_links = ['title']
 
-    def count_text(self, obj):
-        return '{}글자'.format(len(obj.text))
+    def count_text(self, post):
+        return '{}글자'.format(len(post.text))
     count_text.short_description='글내용 글자수'
     
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list_response(request):
+    name = 'Django'
+    response = HttpResponse(f'<h2>Hello {name}!!</h2>', content_type="text/html")
+    response.write(f'<h2>Hello {name}!!</h2>')
+    response.write(f'<p>HTTP Method : {request.method}</p>')
+    response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
+    #return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
+    return response
+
+# Post 목록
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
