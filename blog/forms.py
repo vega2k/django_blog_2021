a7e584df1b8a52752a8da538d204e1ec1ac78b0f
vega2k <vega2k@naver.com>
2021-01-14 17:41:54 +0900

    Comment form add

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
 
 class PostModelForm(forms.ModelForm):
     class Meta:
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
-from .forms import PostModelForm, PostForm
+from .forms import PostModelForm, PostForm, CommentForm
+
+# Comment 등록
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form':form})
 
 # Post 삭제
 @login_required
