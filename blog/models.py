37f85794969fa4b0316039326b7c1b7729d0b9ab
vega2k <vega2k@naver.com>
2021-01-15 11:10:29 +0900

    comment approve remove

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,10 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    # 승인된 댓글만 필터링
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
 # Post 에 달린 댓글
 class Comment(models.Model):
