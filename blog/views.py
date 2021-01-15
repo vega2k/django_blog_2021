f6bc2eef366aae3ff8c143315828b43f9a43df79
vega2k <vega2k@naver.com>
2021-01-15 15:02:36 +0900

    pagination

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Comment
 from .forms import PostModelForm, PostForm, CommentForm
@@ -107,7 +108,18 @@ def post_detail(request, pk):
 
 # Post 목록
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(post_list, 2)
+    page_no = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page_no)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_list_response(request):
